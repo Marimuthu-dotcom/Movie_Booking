43ab316bfd995c33f63511ba49a9b0d26985d948
Marimuthu <maripavin7@gmail.com>
2026-02-05 11:49:07 +0530

    Nothing to add

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql2");
+require('dotenv').config(); 
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "marimuthu78@",
-  database: "ticket_booking"
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database:process.env.MYSQL_DATABASE,
+  port:process.env.MYSQL_PORT,
+  ssl:{rejectUnauthorized:false}
 });
 
 db.connect((err) => {
