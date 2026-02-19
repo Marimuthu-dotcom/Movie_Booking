e7cdaf72507f3bd18debd72d7dad9e63237dd9f4
Marimuthu <maripavin7@gmail.com>
2026-02-19 22:03:10 +0530

    Using Brevo in changes

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -37,6 +37,6 @@ exports.signup = async (req, res) => {
 
   } catch (error) {
     console.log("Signup error:", error);
-    res.status(500).json({ error: "Otp is not sent successfully" });
+    res.status(500).json({ error: "Otp is not sending successfully" });
   }
 };
