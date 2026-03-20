e7540d8bcaca86dac778b1e5194ceaf6133709da
Marimuthu <maripavin7@gmail.com>
2026-03-20 14:35:46 +0530

    Some changes in this code

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -163,7 +163,7 @@ exports.login = async (req, res) => {
 
 exports.getPreviousData = async (req, res) => {
   try {
-    const authHeader = req.headers.Authorization;
+    const authHeader = req.headers.authorization;
 
     if (!authHeader) {
       return res.status(401).json({
